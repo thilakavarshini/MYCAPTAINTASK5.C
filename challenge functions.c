8f3dcdc2c9b032ddd51e6a79acb3a278b9ec896d
thilakavarshini <67496110+thilakavarshini@users.noreply.github.com>
2020-06-30 19:14:33 +0530

    created challenge functions

---
+++ challenge functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sum(int n);
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Sum = %d", sum(num));
+    return 0;
+}
+
+int sum(int n) {
+    if (n != 0)
+        return n + sum(n - 1);
+    else
+        return n;
+}
+
